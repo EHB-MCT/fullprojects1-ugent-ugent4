fcebf3dfc8c26a5616c62bad8c45e610f52fd3b3
MaximWesterbeek <114074573+MaximWesterbeek@users.noreply.github.com>
2023-02-01 09:34:44 +0100

    Participation update

--- vote.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-    document.getElementById("menu").style.display = "block";
-};
-
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    menu.classList.toggle("menu-visible");
-}
