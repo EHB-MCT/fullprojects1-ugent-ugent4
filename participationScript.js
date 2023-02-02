255ac8da07ab2868a489832475bfce0e5783cc48
MaximWesterbeek <114074573+MaximWesterbeek@users.noreply.github.com>
2023-02-02 22:57:52 +0100

    Participation update

--- participationScript.js
+++ participationScript.js
@@ -16,6 +16,14 @@ const navLink2 = document.querySelectorAll('.nav-item .nav-button-2').forEach(li
 })
 
 
+function onlyOne(radio) {
+  var checkboxes = document.getElementsByName('check')
+  checkboxes.forEach((item) => {
+      if (item !== checkmark) item.checked = false
+  })
+}
+
+
 // Hide hamburger-menu on scroll
 //var prevScrollpos = window.pageYOffset;
 //window.onscroll = function() {
